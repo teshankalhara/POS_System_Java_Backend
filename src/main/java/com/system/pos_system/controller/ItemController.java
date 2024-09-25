aee0a4d7625d3c97c944e406693c17db2607faf0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-09-26 02:46:19 +0530

    controller item

--- src/main/java/com/system/pos_system/controller/ItemController.java
+++ src/main/java/com/system/pos_system/controller/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,5 +75,14 @@ public class ItemController {
 
         itemService.updateItem(id, updatedItem);
         return ResponseEntity.status(201).body("Updated Item!");
+    }
+
+    @DeleteMapping("/item/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id) {
+        if (itemService.getItemById(id) == null) {
+            return ResponseEntity.status(404).body("Item Not Found!");
+        }
+        itemService.deleteItem(id);
+        return ResponseEntity.status(201).body("Item Deleted!");
     }
 }
