b2fa18615915b71e1469d2fcc562ff2aaa1e215c
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-09-26 02:56:29 +0530

    controller item_category

--- src/main/java/com/system/pos_system/controller/ItemCategoryController.java
+++ src/main/java/com/system/pos_system/controller/ItemCategoryController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.system.pos_system.dto.ItemCategoryReqDTO;
 import com.system.pos_system.entity.ItemCategory;
 import com.system.pos_system.service.ItemCategoryService;
 
@@ -31,5 +34,25 @@ public class ItemCategoryController {
     @PostMapping("/category")
     public ResponseEntity<ItemCategory> createItemCategory(@RequestBody ItemCategory itemCategory) {
         return ResponseEntity.status(201).body(itemCategoryService.createItemCategory(itemCategory));
+    }
+
+    @PutMapping("/category/{id}")
+    public ResponseEntity<String> updateItemCategory(@PathVariable Long id,
+            @RequestBody ItemCategoryReqDTO itemCategoryReqDTO) {
+        if (itemCategoryReqDTO.getName() == null || itemCategoryReqDTO.getName() == "") {
+            return ResponseEntity.status(422).body("Item Category Name Missing!");
+        }
+        ItemCategory itemCategory = new ItemCategory();
+        itemCategory.setName(itemCategoryReqDTO.getName());
+        itemCategoryService.updateItemCategory(id, itemCategory);
+        return ResponseEntity.status(201).body("Updated Item Category!");
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<String> deleteItemCategory(@PathVariable Long id) {
+        if (itemCategoryService.getItemCategoryById(id) == null) {
+            return ResponseEntity.status(404).body("Item Category Not Found!");
+        }
+        return ResponseEntity.status(201).body("Deleted Item Category!");
     }
 }
---
+++ src/main/java/com/system/pos_system/dto/ItemCategoryReqDTO.java
@@ -0,0 +1,10 @@
+package com.system.pos_system.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ItemCategoryReqDTO {
+    private String name;
+}
