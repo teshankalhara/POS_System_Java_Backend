31021040b2de4f031f44d281a8cb73e631867bfd
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-09-25 18:18:49 +0530

    repository

---
+++ src/main/java/com/system/pos_system/repository/StockRepository.java
@@ -0,0 +1,11 @@
+package com.system.pos_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.system.pos_system.entity.Stock;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
+
+}
