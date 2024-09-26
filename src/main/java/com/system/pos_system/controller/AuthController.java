6c537a77486be7c32061e3eea74a1f6880b49aee
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-09-27 01:09:32 +0530

    auth

--- src/main/java/com/system/pos_system/controller/AuthController.java
+++ src/main/java/com/system/pos_system/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.system.pos_system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,11 +26,23 @@ public class AuthController {
     private JwtUtils jwtUtils;
 
     @PostMapping("/login")
-    public String login(@RequestBody User user) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwtToken = jwtUtils.generateJwtToken(authentication);
-        return jwtToken;
+    public ResponseEntity<?> login(@RequestBody User user) {
+        if (user.getUsername() == null || user.getUsername() == "") {
+            return ResponseEntity.status(400).body("Username Required!");
+        }
+        if (user.getPassword() == null || user.getPassword() == "") {
+            return ResponseEntity.status(400).body("Password Required!");
+        }
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwtToken = jwtUtils.generateJwtToken(authentication);
+            return ResponseEntity.status(200).body(jwtToken);
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(401).body("Authentication Failed!");
+        } catch (Exception e) {
+            return ResponseEntity.status(403).body("Username or Password Incorrect!");
+        }
     }
 }
