41feb450ca476611470685b2de69267fd51f24d1
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-09-26 22:37:46 +0530

    user security,controller

--- src/main/java/com/system/pos_system/service/UserServiceImpl.java
+++ src/main/java/com/system/pos_system/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.system.pos_system.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.system.pos_system.entity.User;
 import com.system.pos_system.repository.UserRepository;
@@ -9,8 +10,12 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
