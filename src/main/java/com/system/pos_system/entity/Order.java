e8021bd663abc1617a942c213464ea176e9299f9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-09-26 20:18:58 +0530

    order

--- src/main/java/com/system/pos_system/entity/Order.java
+++ src/main/java/com/system/pos_system/entity/Order.java
@@ -2,16 +2,34 @@ package com.system.pos_system.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime orderDateTime;
+
+    @Column(nullable = false)
     private Double totalPrice;
 
+    @PrePersist
+    protected void onCreate() {
+        if (this.orderDateTime == null) {
+            this.orderDateTime = LocalDateTime.now();
+        }
+    }
 }
